870c8f26ccfafe6971870d2ad823b780b48ab82b
Milton Villar <milton.suscripciones@gmail.com>
2023-03-17 20:37:06 -0300

    se añadio administrador y clase post

--- MiApp/models.py
+++ MiApp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=30)
+    descripcion_titulo = models.CharField(max_length=80)
+    subtitulo = models.CharField(max_length=10)
+    descripcion = models.CharField(max_length= 150)
+
+    def __str__(self) :
+        return f"{self.id} - {self.titulo}"
+
+
+
+
