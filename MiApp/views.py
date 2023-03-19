758fc15f2c5ecde4d2dbcfb97baa75dfba8e26c5
Milton Villar <milton.suscripciones@gmail.com>
2023-03-19 14:38:32 -0300

    Se corrigio el metodo POST en templates y view.py (antes tenia GET)

--- MiApp/views.py
+++ MiApp/views.py
@@ -16,7 +16,7 @@ def compras (request):
     return render(request, "MiApp/compras.html", context)
 
 def agregar_compra (request):
-    compra_form = CompraForm(request.GET)
+    compra_form = CompraForm(request.POST)
     compra_form.save()
     context = {"form":CompraForm(),}
     return render(request, "MiApp/compras.html", context)
@@ -26,7 +26,7 @@ def proveedores (request):
     return render(request, "MiApp/proveedores.html", context)
 
 def agregar_proveedor (request):
-    proveedor_form = ProveedorForm(request.GET)
+    proveedor_form = ProveedorForm(request.POST)
     proveedor_form.save()
     context = {"form":ProveedorForm(),}
     return render(request, "MiApp/proveedores.html", context)
@@ -36,7 +36,7 @@ def recursos (request):
     return render(request, "MiApp/recursos.html",context)
 
 def agregar_recurso (request):
-    recurso_form = RecursoForm(request.GET)
+    recurso_form = RecursoForm(request.POST)
     recurso_form.save()
     context = {"form":RecursoForm(),}
     return render(request, "MiApp/recursos.html", context)
